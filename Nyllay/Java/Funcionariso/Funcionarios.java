f1c20647f03ed1b8764e333fa822c1bbcbaec7aa
Nyllay <garoto0115hg@gmail.com>
2025-08-01 20:03:11 -0400

    Funcionarios do banco

---
+++ Nyllay/Java/Funcionariso/Funcionarios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.funcionarios;
+
+/**
+ *
+ * @author Aluno01
+ */
+public class Funcionarios {
+            
+            String nome;
+            String departamento;
+            double salario;
+            String dataentrada;
+            String RG;
+            
+            
+            void metre(){
+              System.out.println("");
+            }
+                    
+}
